ed37c55d921937db6b13845f871b239992434367
syahminetgeo <syahmi@netgeometry.com>
2022-09-01 12:25:50 +0800

    Redirect link to DataGeometry

--- src/components/Configurator/Function.vue
+++ src/components/Configurator/Function.vue
@@ -395,7 +395,7 @@ export default {
     documentationURL () {
       // eslint-disable-next-line no-undef
       const [year, month] = VERSION.split('.')
-      return `https://docs.cortezaproject.org/corteza-docs/${year}.${month}/integrator-guide/expr/index.html`
+      return `https://datageometry.io`
     },
   },
 
